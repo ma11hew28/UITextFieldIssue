b7662a241fce82e0d8f66518aa7b565b30bc472e
Matt Di Pasquale <liveloveprosper@gmail.com>
2011-11-26 01:21:32 -0500

    Add sampleTextField w callbacks to ViewController.

--- UITextFieldIssue/ViewController.h
+++ UITextFieldIssue/ViewController.h
@@ -1,5 +1,7 @@
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITextFieldDelegate>
+
+@property (strong, nonatomic) UITextField *sampleTextField;
 
 @end
--- UITextFieldIssue/ViewController.m
+++ UITextFieldIssue/ViewController.m
@@ -1,52 +1,56 @@
 #import "ViewController.h"
 
+@interface ViewController ()
+- (void)textFieldDidChange:(UITextField *)textField;
+@end
+
 @implementation ViewController
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Release any cached data, images, etc that aren't in use.
-}
+@synthesize sampleTextField;
 
-#pragma mark - View lifecycle
+#pragma mark - UIViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)viewDidUnload
-{
+- (void)viewDidUnload {
+    self.sampleTextField = nil;
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
 }
 
-- (void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
+- (void)viewDidLoad {
+    sampleTextField = [[UITextField alloc] initWithFrame:CGRectMake(20.0f, 20.0f, 100.0f, 44.0f)];
+    sampleTextField.delegate = self;
+    [sampleTextField addTarget:self action:@selector(textFieldDidChange:)
+              forControlEvents:UIControlEventEditingChanged];
+    sampleTextField.backgroundColor = [UIColor yellowColor];
+    [self.view addSubview:sampleTextField];
+    [sampleTextField becomeFirstResponder];
+
+    sampleTextField.text = @"b"; // setting text here doesn't trigger textFieldDidChange:
+
+    [super viewDidLoad];
 }
 
-- (void)viewDidAppear:(BOOL)animated
-{
-    [super viewDidAppear:animated];
+// The code below should make it so that if sampleTextField.text matches "b", then whatever I type
+// next should replace "b". Instead, it's appended to "b" because calling setText: in
+// textField:shouldChangeCharactersInRange:replacementString: faultily triggers textFieldDidChange:.
+
+#pragma mark - UITextFieldDelegate
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range
+replacementString:(NSString *)string {
+    if ([sampleTextField.text isEqualToString:@"b"]) {
+        if ([string length]) {          // inserting
+            sampleTextField.text = @""; // setting text here triggers textFieldDidChange:
+        }
+    }
+    return YES;
 }
 
-- (void)viewWillDisappear:(BOOL)animated
-{
-	[super viewWillDisappear:animated];
-}
+#pragma mark - UIControlEventEditingChanged
 
-- (void)viewDidDisappear:(BOOL)animated
-{
-	[super viewDidDisappear:animated];
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    // Return YES for supported orientations
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+- (void)textFieldDidChange:(UITextField *)textField {
+    if (![sampleTextField hasText]) {
+        sampleTextField.text = @"b";
+    }
 }
 
 @end
